5560e0926e33c2182d18e51b99b9bed904c166fc
anjorlar <adeanjola@gmail.com>
2022-01-26 12:58:17 +0100

    changes to config.js

--- server/config/config.js
+++ server/config/config.js
@@ -10,19 +10,9 @@ module.exports = {
         dialect: 'postgres',
     },
 
-    test: {
-        database: process.env.TEST_DB_NAME,
-        username: process.env.TEST_DB_USERNAME,
-        password: process.env.TEST_DB_PASSWORD,
-        host: process.env.TEST_DB_HOST,
-        dialect: 'postgres',
-    },
-
     production: {
-        database: process.env.DB_NAME,
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        host: process.env.DB_HOST,
-        dialect: 'postgres',
+        use_env_variable: process.env.DATABASE_URL,
+
+
     },
 };
