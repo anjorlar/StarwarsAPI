a4a22a7c84ea29b43c22c9038504fb4237cde926
anjorlar <adeanjola@gmail.com>
2022-01-26 18:25:36 +0100

    changes to config files

--- server/config/config.js
+++ server/config/config.js
@@ -11,6 +11,11 @@ module.exports = {
     },
 
     production: {
-        DATABASE_URL: process.env.DATABASE_URL,
+        database: process.env.DB_NAME,
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: 'postgres',
     },
 };
